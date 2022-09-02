0803ba6de130ea6dce8c5700f73d031315e38dad
Stanslous <stancemuindi21@gmail.com>
2022-09-02 07:01:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = "not bob";
+const leastFavoriteCustomer ="XYZ";
+
+function upperCaseCustomerName(){
+    let stance = customerName.toUpperCase();
+    return stance;
+}
+//console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+   return bestCustomer;
+}
+
+
+function overwriteBestCustomer(){
+ bestCustomer = "maybe bob";
+}
+console.log(overwriteBestCustomer())
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer  = 'Assignment to constant variable.'
+    return leastFavoriteCustomer ;
+} 
+console.log(changeLeastFavoriteCustomer());
